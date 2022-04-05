1a81d5c3fafff5f277b1d4a3134220c1424b3996
ruanvalente <ruanopensuselinux@gmail.com>
2022-04-05 15:39:35 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import createEmotionCache from "../utils/createEmotionCache";
+import theme from "../theme/theme";
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+export default function Home() {
+  return (
+    <>
+      <Button variant="contained" color="secondary">
+        Enviar
+      </Button>
+    </>
+  );
+}
